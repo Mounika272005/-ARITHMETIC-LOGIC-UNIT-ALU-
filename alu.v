476a062363f75e4347c273460b7b3a32b9befd37
Mounika272005 <byreddym9949@gmail.com>
2025-06-16 14:04:31 +0530

    Add files via upload

--- alu.v
+++ alu.v
@@ -1,9 +1,18 @@
-module alu(input [7:0] a, b, input [1:0] sel, output reg [7:0] result);
-    always @(*) begin
-        case(sel)
-            2'b00: result = a + b;
-            2'b01: result = a - b;
-            default: result = 8'h00;
-        endcase
-    end
+module alu (
+    input [3:0] A, B,
+    input [2:0] sel,
+    output reg [3:0] result
+);
+
+always @(*) begin
+    case (sel)
+        3'b000: result = A + B;      // ADD
+        3'b001: result = A - B;      // SUB
+        3'b010: result = A & B;      // AND
+        3'b011: result = A | B;      // OR
+        3'b100: result = ~A;         // NOT A
+        default: result = 4'b0000;
+    endcase
+end
+
 endmodule
